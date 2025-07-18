f31b4ad7bb8c74af5946aaf8d1734b1441e3bc82
swalih934 <sswalih881@gmail.com>
2025-07-18 17:36:02 +0530

    'first'

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState,useContext } from 'react'
+import { Routes,Route } from 'react-router-dom'
+import './App.css'
+import Auth from './pages/Auth'
+import Home from './pages/Home'
+import Agenta from './pages/Agenta'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
+import { ThemeContext } from './context/ThemeProvider'
+import { Link, useLocation } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const location=useLocation()
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+  console.log(location.pathname);
+
+  return (
+    <>
+     <nav className="p-3 d-flex justify-content-between align-items-center ">
+  <div className="fw-bold"></div>
+
+  <button
+    onClick={toggleDarkMode}
+    className={`btn btn-sm btn-outline-primary ms-auto ${location.pathname === '/' ? 'd-none' : ''}`}
+    >
+    {darkMode ? '☀ Light Mode' : '🌙 Dark Mode'}
+  </button>
+</nav>
+    <Routes>
+      <Route path='/' element={<Auth/>}/>
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/agenta' element={<Agenta/>}/>
+
+    </Routes>
+    <ToastContainer/>
+    </>
+  )
+}
+
+export default App
